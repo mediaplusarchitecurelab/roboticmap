c6155469271eac059fb6faa5cd25f55f204b730f
chifuresearch <45413636+chifuresearch@users.noreply.github.com>
2018-12-03 08:06:18 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -28,25 +28,75 @@ const buttonMapping = {
 
 
 
-const ssurlp = 'https://spreadsheets.google.com/feeds/list/1g4CAWGxpcj9CbwMNY_A5BGtmSlZ8bbMdexVREEyJXOU/1/public/values?alt=json';
+const ssurlp = 'https://spreadsheets.google.com/feeds/list/1-StkrwSEzLoA5s3AlcRp0RGJlSyU4_uwf4sz9fOB2yE/4/public/values?alt=json';
 const ssurlloc = 'https://spreadsheets.google.com/feeds/list/1g4CAWGxpcj9CbwMNY_A5BGtmSlZ8bbMdexVREEyJXOU/2/public/values?alt=json';
+const ssurllocmap = 'https://spreadsheets.google.com/feeds/list/1-StkrwSEzLoA5s3AlcRp0RGJlSyU4_uwf4sz9fOB2yE/3/public/values?alt=json';
 
-const exeurl = 'https://script.google.com/macros/s/AKfycbxCm1DwMr4aguILPVMg9-L9Wh5GPT2JijZE_Fe2JDnbFaL6kuE/exec';
+const exeurl = 'https://script.google.com/macros/s/AKfycbxiLkwaCbVOkHBkk-38Nd69QkzvRswegF3KyCHhxHQdyHwmbmo/exec';
 const bgpath = 'assets/img/bg.jpg';
 
 const dot = new DOT(400,300);
 const recording = new RECORD();
 const layout = new LAYOUT();
 
+var canv;
+
 var timestamp=0;
 var basemap={};
+//var locmap=[];
+
+
+
+
+
+// gui params
+var numShapes = 20;
+var strokeWidth = 4;
+var strokeColor = '#00ddff';
+var fillColor = [180, 255, 255];
+var drawStroke = true;
+var drawFill = true;
+var radius = 20;
+var shape = ['circle', 'triangle', 'square', 'pentagon', 'star'];
+var label = 'label';
+// gui
+var visible = true;
+var gui, gui2;
+// dynamic parameters
+var bigRadius;
+
+var selecteditem=false;
+var overloc=false;
+
+var exportval='';
+
+var feedback='';
+
+
+
 
 function preload() {
   
   loadJSON(ssurlloc, encodeloc);
   loadJSON(ssurlp, encodep);
-  layout.setbg(loadImage(bgpath));
-  layout.setmap(basemap);
+  loadJSON(ssurllocmap, encodelocmap);
+}
+function encodelocmap(data){
+      let vs = [];
+
+      for (let i = 0; i < data.feed.entry.length; i+=1) {
+        let val=[]
+        let vlist = data.feed.entry[i].content.$t.split(',');
+        console.log(vlist.length);
+        for(let j=0; j<vlist.length; j+=1){
+            let v=vlist[j];
+            val.push(int(v.split(': ')[1]));
+        }
+        vs.push(val);
+      }
+      //console.log(vs);
+      //pmap=vs;
+      basemap.locationmap=vs;
 }
 
 function encodeloc(data){
@@ -63,8 +113,10 @@ function encodeloc(data){
             		};
     		vs.push(v);
   		}
+      locmap=vs;
   		basemap.location=vs;
 }
+
 function encodep(data){
   		let vs = [];
 
@@ -79,67 +131,43 @@ function encodep(data){
                        data.feed.entry[i].gsx$地點8.$t,
                        data.feed.entry[i].gsx$地點9.$t,
                        data.feed.entry[i].gsx$地點10.$t];
-            //console.log('assets/img/person/'+);
-
-            //image(ppic, 0, 0, 40, 60);
-
-    		let  v= {	"id": data.feed.entry[i].gsx$瑞安編號.$t,
-    					"behavior": data.feed.entry[i].gsx$屬性.$t,
+        
+    		let  v= {	  "id": data.feed.entry[i].gsx$瑞安編號.$t,
+    					      "behavior": data.feed.entry[i].gsx$屬性.$t,
                 		"rule": rule,
-                		"current": data.feed.entry[i].gsx$現在位置.$t,
+                		"currentX": data.feed.entry[i].gsx$索引橫.$t,
+                    "currentY": data.feed.entry[i].gsx$索引直.$t,
                 		"pic": data.feed.entry[i].gsx$圖片.$t
             		};
+
     		vs.push(v);
   		}
+      //pmap=vs;
   		basemap.person=vs;
 }
-/*
-function encodep(data){
-  		let vs = [];
-
-  		for (let i = 0; i < data.feed.entry.length; i+=1) {
-    		let p = {   "x": int(data.feed.entry[i].gsx$座標x.$t),
-                		"y": int(data.feed.entry[i].gsx$座標y.$t)
-            		};
-    		let  v= {
-                		"position": p,
-                		"diameter": int(data.feed.entry[i].gsx$範圍.$t),
-                		"descript": data.feed.entry[i].gsx$說明.$t,
-            		};
-    		vs.push(v);
-  		}
-  		basemap.location=vs;
-}
-*/
 
 function setup() {
   //imageMode(CENTER);
   createCanvas(1200,900);
+  // after loading
+  layout.setbg(loadImage(bgpath));
+  layout.setmap(basemap);
+
   textAlign(LEFT,CENTER);
   ellipseMode(CENTER);
-  //loop();
-  //dot=
+  // Calculate big radius
+  bigRadius = height / 3.0;
+/*
+  // Create Layout GUI
+  gui = createGui('P5 GUI');
+  gui.addGlobals('numShapes', 'bigRadius', 'shape', 'label', 'radius',
+  'drawFill', 'fillColor', 'drawStroke', 'strokeColor', 'strokeWidth');
+*/
 }
 
 function draw() {
-  
-  
 
   let buttonCount = 0;
-  //for(let p = 0; p <= buttonNum; p++) {
-    //const name = buttonMapping[p] || p;
-
-    /*
-    if(isNaN(name)){
-      fill(0);
-      text(name, 100, 25 * buttonCount + 30);
-      noFill();
-      ellipse(85, 25 * buttonCount + 30, 13, 13);
-      buttonCount++;
-    }
-    */
-  //}
-
   let gamepads = navigator.getGamepads ? navigator.getGamepads() : [];
   let gamepadArray = [];
 
@@ -166,6 +194,7 @@ function draw() {
       }
     }
   }
+
   orderedGamepads.push(gamepadArray[xbox]);
   //orderedGamepads.push(gamepadArray[joyconL]);
   let pressed = [];
@@ -248,7 +277,18 @@ function draw() {
     
 }
 
+function mouseReleased(){
+  if (selecteditem.type === 'PERSON'){
 
+    if (overloc){
+      //console.log(overloc);
+      selecteditem.setLocid(overloc);
+    }else{
+      //console.log('dd');
+    }
+  }
+  selecteditem=false;
+}
 
 function getIndex(id) {
   var resultIndex = 0
@@ -273,53 +313,274 @@ function DOT(x,y){
   }
 
   this.display = function(){
-    
+    /*
     noStroke();
     fill(200, 0, 0);
     ellipse(this.x, this.y, this.diameter, this.diameter);
+    */
   }
 }
+/*
+function LOCMAP(map){
+  this.map=map;
+  
+  this.display = function(){
+      for(let i=0;i<this.map.length;i+=1){
+        for(let j=0;j<this.map[i].length;j+=1){
+
+        }
+      }
+  }
+
+}
+*/
+function LOCDOT(xid,yid,st,w=15,h=15){
+  this.xid=xid;
+  this.yid=yid;
+  this.status=st;
+
+  this.w= w;
+  this.h= h;
+  this.d= this.h*0.8;
+
+  this.x=(this.xid+0.5)*this.w;
+  this.y=(this.yid+0.5)*this.h;
+  this.lux=this.x-(this.w/2);
+  this.luy=this.y-(this.h/2);
+
+  this.over=false;
+  //this.status=0;
+  //console.log('a');
+  
+  this.setW=function(w){
+    this.w=w;
+    this.d=this.w*0.8;
+    this.x=(this.xid+0.5)*this.w;
+    this.lux=this.x-(this.w/2);
+  }
+  this.setH=function(h){
+    this.w=h;
+    this.y=(this.yid+0.5)*this.h;
+    this.luy=this.y-(this.h/2);
+  }
+  this.setD=function(d){
+    this.d=d;
+  }
+  /* 
+    status = 0 >>   不出現
+    status = 1-9 >> 地點
+    status > 10 >>  上有人
+    
+  */
+  this.locmapEvent = function(st){
+    if (st === 0){}
+    else{
+      this.over = collidePointRect(mouseX,mouseY,this.lux,this.luy,this.w, this.h);
+      fill(0);
+      if (this.over){
+        overloc=this; 
+        //text('('+overloc.x+','+overloc.y+')', 100,100);
+        noStroke();
+        fill(50, 0, 50,225);
+            
+      }else{
+        //text(this.descript, this.x+this.diameter,  this.y-(this.diameter/2));
+
+        noStroke();
+        switch (this.status){
+          case 1:
+            fill(200, 200, 0,100);
+            break;
+          case 2:
+            fill(200, 0, 200,100);
+            break;
+          case 3:
+            fill(100, 0, 200,100);
+            break;
+          case 4:
+            fill(200, 100, 0,100);
+            break;
+          case 5:
+            fill(100, 0, 100,100);
+            break;
+          case 6:
+            fill(200, 0, 0,100);
+            break;
+          case 7:
+            fill(0, 0, 200,100);
+            break;
+          default:
+            fill(200, 200, 200,100);
+            break;
+        }
+      }
+      ellipse(this.x,this.y,this.d,this.d);
+    }
+  }
+  this.display = function(){
+    this.locmapEvent(this.status);
+    
+  }
+
+}
+
+function PERSON(linklocdot){
+  this.type='PERSON';
+  this.layout=layout;
+  this.linklocdot=linklocdot;
+  //console.log(locmap);
+  this.x = this.linklocdot.x;
+  this.y = this.linklocdot.y;
+
+  this.diameter = 30;
+  this.diametertol = 15;
+
+  this.over=false;
+  this.selected=false;
+  this.id=0;
+  this.behavior='';
+  this.rule=[];
+  this.pic='';
+  this.printoutdata='';
+
+  this.setRule = function(rule){
+    this.rule=rule;
+  }
+  this.setId = function(id){
+    this.id=id;
+  }
+  this.setLocid = function(linklocdot){
+    // old
+      this.printoutdata= this.linklocdot.x + ',' + this.linklocdot.y+ ',';
+    // new
+    this.linklocdot=linklocdot;
+    this.x = this.linklocdot.x;
+    this.y = this.linklocdot.y;
+      this.printoutdata+= this.x + ',' + this.y;
+
+    // 輸出到spreadsheet
+    
+          var exportout = {
+                  data: this.printoutdata,
+                  sheetUrl: 'https://docs.google.com/spreadsheets/d/1-StkrwSEzLoA5s3AlcRp0RGJlSyU4_uwf4sz9fOB2yE//edit?usp=sharing',
+                sheetTag: '手臂動作'
+          };
+          $.get(exeurl, exportout);
+
+    alert('uploaded'+'</br>'+this.printoutdata);
+
+ /*   
+    console.log(this.rule);
+
+    this.locid=locid;
+    this.x = this.locmap.location[this.locid].position.x;
+    this.y = this.locmap.location[this.locid].position.y;
+*/
+  //console.log(overloc.x+','+overloc.y);
+  }
+  this.setBehavior = function(be){
+    this.behavior=be;
+  }
+  this.setPic = function(pic){
+    this.pic=pic;
+  }
+  this.mosueEvent = function(){
+    this.over = collidePointCircle(mouseX,mouseY,this.x,this.y,this.diameter+this.diametertol);
+
+    if (this.over){
+      noStroke();
+      fill(200, 0, 0);
+      this.diametertol = 8;
+      if (mouseIsPressed){
+        this.x=mouseX;
+        this.y=mouseY;
+        selecteditem=this;  
+      }
+    }else{
+      noStroke();
+      fill(50, 50, 50);
+      this.diametertol = 0;
+    }
+  }
+
+  this.display = function(){
+    this.mosueEvent();
+    text(this.behavior, this.x+this.diameter,  this.y);
+    ellipse(this.x, this.y, this.diameter+this.diametertol, this.diameter+this.diametertol);
+  }
+}
+
 function LAYOUT(){
-	this.bg;
-	this.map;
-	
+  this.bg;
+  this.map={};
+  this.pmap=[];
+  this.locmap=[];
+  this.xbound=1;
+  this.ybound=1;
+  this.locw=1;
+  this.loch=1;
 
-	this.setbg=function(bg){
-		this.bg=bg;
-	}
-	this.setmap=function(map){
-		this.map=map;
-	}
-	this.display =function(){
-		background(this.bg);
-		for(let i=0;i<this.map.location.length;i+=1){
-				let val = this.map.location[i];
-				noStroke();
-	          	fill(200, 0, 200,50);
-				ellipse(val.position.x,val.position.y,val.diameter,val.diameter);
-			}
-		for(let i=0;i<this.map.person.length;i+=1){
-				let val = this.map.person[i];
-				let p = int(split(val.current,'地點')[1]);
+  this.setbg=function(bg){
+    this.bg=bg;
+  }
 
-				noStroke();
-	          	fill(50, 50, 50);
-				ellipse(this.map.location[p].position.x,this.map.location[p].position.y,20,20);
-			}
-		/*
-		
-		if (typeof this.map.location === 'undefined'){}
-		else{
-			console.log(this.map.location);
-			for(let i=0;i<this.map.location.length;i+=1){
-				let val = this.map.location[i];
-				noStroke();
-	          	fill(200, 0, 200,50);
-				ellipse(val.position.x,val.position.y,val.diameter,val.diameter);
-			}
-		}
-		*/
-	}
+  this.setmap=function(map){
+    this.map=map;
+    // LOCATION
+    /*
+    for(let i=0;i<this.map.location.length;i+=1){
+
+        let val = new LOCATION(i,this.map);
+        val.setDiameter(this.map.location[i].diameter);
+        val.setPic(this.map.location[i].pic);
+        
+        this.locmap.push(val);
+    }
+    */
+    // LOCATIONMAP
+    this.ybound = this.map.locationmap.length;
+    this.loch = height/this.ybound;
+    this.xbound = this.map.locationmap[0].length;
+    this.locw = width/this.xbound;
+
+    for(let i=0;i<this.ybound;i+=1){
+        
+        for(let j=0;j<this.xbound;j+=1){
+          this.locmap.push(new LOCDOT(j,i,map.locationmap[i][j],this.locw,this.loch));
+        }
+    }
+    // PERSON
+    for(let i=0;i<this.map.person.length;i+=1){
+        let locidx = int(this.map.person[i].currentX);
+        let locidy = int(this.map.person[i].currentY);
+        console.log(this.locmap[locidx+this.xbound*locidy]);
+        
+
+        let val = new PERSON(this.locmap[locidx+this.xbound*locidy]);
+        val.setRule( this.map.person[i].rule );
+        val.setId(this.map.person[i].id);
+        val.setBehavior(this.map.person[i].behavior);
+        val.setPic(this.map.person[i].pic);
+        
+        this.pmap.push(val);
+    }
+  }
+  this.getIndexlocdot =function(xid,yid){
+    return this.locmap[xid+yid*xbound];
+  }
+  this.display =function(){
+    background(225);
+    for(let i=0;i<this.locmap.length;i+=1){
+        let val = this.locmap[i];
+        val.display();
+      }
+    
+    for(let i=0;i<this.pmap.length;i+=1){
+        let val = this.pmap[i];
+        val.display();
+      }
+    
+  }
 }
 
 function RECORD(){
@@ -369,6 +630,11 @@ function RECORD(){
             
           }
       }
+
+    noStroke();
+    fill(0);
+    text(feedback, 100,  130);
+/*
     //text
     noStroke();
     fill(0);
@@ -390,19 +656,9 @@ function RECORD(){
     	//console.log(dot.x);
     	if (this.mode===0){
     		//執行upload
-    		/*
-    		let psstr = dot.x+','+dot.y+',0,0';
-	    		let exportout = {
-	                  data: psstr,
-	                  sheetUrl: 'https://docs.google.com/spreadsheets/d/108xNAjSV8PCimIzfbbyYG2FrsNYRVl64vfxrwd-omCg/edit?usp=sharing',
-	                  sheetTag: '手臂位置'
-	          	};
-	    		$.get(exeurl, exportout);
-    		
-    		
-			*/
 		}
     }
+*/
   }
   this.export = function(){
     if (this.upload){
@@ -440,3 +696,107 @@ function RECORD(){
     }
   }
 }
+
+/*
+
+// check for keyboard events
+function keyPressed() {
+  switch(key) {
+    // type [F1] to hide / show the GUI
+    case 'p':
+      visible = !visible;
+      if(visible) gui.show(); else gui.hide();
+      break;
+  }
+}
+
+
+// draw a regular n-gon with n sides
+function ngon(n, x, y, d) {
+  beginShape();
+  for(var i = 0; i < n; i++) {
+    var angle = TWO_PI / n * i;
+    var px = x + sin(angle) * d / 2;
+    var py = y - cos(angle) * d / 2;
+    vertex(px, py);
+  }
+  endShape(CLOSE);
+}
+
+
+// draw a regular n-pointed star
+function star(n, x, y, d1, d2) {
+  beginShape();
+  for(var i = 0; i < 2 * n; i++) {
+    var d = (i % 2 === 1) ? d1 : d2;
+    var angle = PI / n * i;
+    var px = x + sin(angle) * d / 2;
+    var py = y - cos(angle) * d / 2;
+    vertex(px, py);
+  }
+  endShape(CLOSE);
+}
+
+function LOCATION(id,locmap){
+  this.type='LOCATION';
+  this.id=id;
+  this.locmap=locmap;
+  //console.log(locmap);
+  this.x = this.locmap.location[this.id].position.x;
+  this.y = this.locmap.location[this.id].position.y;
+
+  this.diameter = 15;
+
+  this.over=false;
+  this.descript = '';
+  this.pic='';
+
+  this.setDiameter = function(d){
+    this.diameter=d;
+  }
+  this.setPic = function(pic){
+    this.pic=pic;
+  }
+  this.setDescript = function(dis){
+    this.descript=dis;
+  }
+  this.personEvent = function(){
+    this.over = collidePointCircle(mouseX,mouseY,this.x,this.y,this.diameter);
+    fill(0);
+    if (this.over){
+      text(this.descript, this.x+this.diameter,  this.y-(this.diameter/2));
+      noStroke();
+      fill(200, 200, 0,120);
+      overloc=this;     
+    }else{
+      text(this.descript, this.x+this.diameter,  this.y-(this.diameter/2));
+      noStroke();
+      fill(200, 200, 0,30);
+    }
+    
+
+    if (this.over){
+      noStroke();
+      fill(200, 0, 0);
+      this.diametertol = 8;
+      if (mouseIsPressed){
+        this.x=mouseX;
+        this.y=mouseY;
+        selecteditem=this;  
+      }
+    }else{
+      noStroke();
+      fill(50, 50, 50);
+      this.diametertol = 0;
+    }
+  }
+
+  this.display = function(){
+    this.personEvent();
+    
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+}
+
+
+*/
